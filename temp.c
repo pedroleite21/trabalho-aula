a36ba815750d52b645a2f8cc2494ae758882b349
Pedro Leite <pedro.leite.001@acad.pucrs.br>
2016-05-22 17:17:54 -0300

    Add files via upload

--- temp.c
+++ temp.c
@@ -3,21 +3,24 @@
 float temperatura (float f) {
 	float c;
 	c = 5.0/9.0 * (f - 32);
-	if ( (c >= -267) && ( c <= -278) ) 
-		printf("(a) Zero Absoluto (-273 ºC)\n");
-	else if ( (c >= - 5) && (c <= 5) ) { 
-		printf("(b) Congelamento da Água (0 ºC)\n");
-	     else if ((c >= 95) && (c <= 105) ) { 
-		printf("(c) Ebulição da Água (100 ºC)\n");}
-	     }
+	
+	if (c >= -267.0 && c <= -278.0) { 
+		printf("(a) Zero Absoluto (-273ºC)\n"); }
+	else if (c >= -5.0 && c<= 5.0) {
+		printf("(b) Congelamento da Água (0ºC)\n"); }
+	     else if (c >= 95.0 && c <= 105.0) {
+		printf ("(c) Ebulição da Água (100ºC)\n");}
 	return c;
 }
 
-int main (void) {
-	float temp;
-		
-	scanf("%f", &temp);
-	temperatura(temp);
+int main() {
+	float f, cel;
+	
+	scanf("%f", &f);
+	
+	cel = temperatura(f);
+
+	printf("Em ºC = %.1f\n", cel);
 
 	return 0;
-}
+}	
